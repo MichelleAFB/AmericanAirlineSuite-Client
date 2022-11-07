a2058f9601bf2e5ba2cdf5c0d28b27702a9d42f8
MichelleAFB <64324539+MichelleAFB@users.noreply.github.com>
2022-11-07 16:53:12 -0600

    recommit
    recommit

--- app/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import {legacy_createStore as createStore } from 'redux'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import {rootReducer} from './rootReducer'
-
-
-
-
-export const store =createStore(rootReducer,composeWithDevTools())
-
